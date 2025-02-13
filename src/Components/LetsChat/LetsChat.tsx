d0f1fbf707aced313f955786c8f57273b2cee46c
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-02-13 21:13:56 +0100

    feat: added lets connect section

---
+++ src/Components/LetsChat/LetsChat.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function LetsChat() {
+	return (
+		<section className="lets-connect-section">
+			<h2>LETS CONNECT</h2>
+			<div className="lets-connect-section-content-wrapper">
+				<p className="lets-connect-section-content-content">Lets chat bro</p>
+				<button className="lets-connect-section-content-button">
+					Pick a time
+				</button>
+			</div>
+		</section>
+	);
+}
