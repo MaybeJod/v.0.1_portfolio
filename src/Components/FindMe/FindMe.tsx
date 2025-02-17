c8742c9c6e29912cb0f4205df86406ac182f29ba
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-02-17 14:46:18 +0100

    feat: added layout for find me section

---
+++ src/Components/FindMe/FindMe.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FindMe() {
+	return (
+		<section>
+			<div className="find-me-section-title">FIND ME</div>
+			<div className="find-me-section-content">Icons</div>
+		</section>
+	);
+}
