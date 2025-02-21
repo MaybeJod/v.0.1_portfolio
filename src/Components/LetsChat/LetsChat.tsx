f01bfe61810106b2779d4f8a4330bc8c3991e57b
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-02-21 14:00:34 +0100

    feat: added standard button to lets chat section

--- src/Components/LetsChat/LetsChat.tsx
+++ src/Components/LetsChat/LetsChat.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import "./LetsChat.css";
+import StandardButton from "../StandardButton/StandardButton";
 
 export default function LetsChat() {
+	function handleClick() {
+		console.log("hello");
+	}
 	return (
 		<section className="lets-connect-section">
-			<h2>LETS CONNECT</h2>
+			<h2 className="lets-connect-title">LETS CONNECT</h2>
 			<div className="lets-connect-section-content-wrapper">
-				<p className="lets-connect-section-content-content">Lets chat bro</p>
-				<button className="lets-connect-section-content-button">
-					Pick a time
-				</button>
+				<p className="lets-connect-section-content-content">
+					Have something you want to pick my brain about? Interested in a
+					collab? Need something from my list of services?
+				</p>
+				<p className="lets-connect-section-content-content">
+					Pick a 15 minute slot, give me a brief intro on what you're looking
+					for, and let's chat.
+				</p>
+				<StandardButton
+					className="lets-connect-section-button"
+					children="Pick a time"
+					ariaLabel="Lets connect section button"
+					onClick={handleClick}
+					tabIndex={0}
+				/>
 			</div>
 		</section>
 	);
