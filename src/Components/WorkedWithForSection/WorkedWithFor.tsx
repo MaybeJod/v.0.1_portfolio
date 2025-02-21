45eb4d90c192779c13370b24bacae08cb7b1e17c
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-02-21 12:18:34 +0100

    style: added fs logo svg in worked with section

--- src/Components/WorkedWithForSection/WorkedWithFor.tsx
+++ src/Components/WorkedWithForSection/WorkedWithFor.tsx
@@ -3,6 +3,7 @@ import "./WorkedWithFor.css";
 import Ion_Game_Design from "../../Icons/Ion_Game_Design";
 import MediPrep_Logo from "../../Icons/MediPrep_Logo";
 import Sodertorn_University_Logo from "../../Icons/Sodertorn_University_Logo";
+import FsLogoSvg from "../../Icons/FsLogoSvg";
 
 export default function WorkedWithFor() {
 	return (
@@ -12,8 +13,8 @@ export default function WorkedWithFor() {
 			</div>
 			<div className="worked-with-for-svgs-wrapper">
 				<Ion_Game_Design />
-
 				<MediPrep_Logo />
+				<FsLogoSvg />
 				<Sodertorn_University_Logo />
 			</div>
 		</section>
