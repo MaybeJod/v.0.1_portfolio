7c79829df2fc453a65efd427a54922966009dafd
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-02-25 17:48:38 +0100

    feat: init on navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,20 @@
-import React from "react";
-
 import "./App.css";
-import ContentSection from "./Components/ContentSection/ContentSection";
-import { ProfileData } from "./Data/DataIndex";
-import TitleSection from "./Components/TitleSection/TitleSection";
-import WorkedWithFor from "./Components/WorkedWithForSection/WorkedWithFor";
-import EducationSection from "./Components/EducationSection/EducationSection";
-import LetsChat from "./Components/LetsChat/LetsChat";
-import FindMe from "./Components/FindMe/FindMe";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Components/Layout/Layout";
+import LandingPage from "./Pages/LandingPage";
+import SummaryPage from "./Pages/SummaryPage";
 
 function App() {
 	return (
 		<div className="App">
-			<TitleSection
-				title="Jod Wongphayak"
-				summary="Stockholm-based Front end dev and UX designer. Currently focused on bridging design and code. Driven by curiosity, mixing analog and digital media to build digital experiences."
-			/>
-			<main className="main-section-wrapper">
-				{/* ... spread operator, it takes all the properties from an object and "spreads them as individual props" */}
-				<ContentSection {...ProfileData.currently} />
-				<ContentSection {...ProfileData.help} />
-				<ContentSection {...ProfileData.past} />
-				<ContentSection {...ProfileData.interests} />
-				<WorkedWithFor />
-				<EducationSection />
-				<FindMe />
-				<LetsChat />
-			</main>
+			<BrowserRouter>
+				<Layout>
+					<Routes>
+						<Route path="/" element={<LandingPage />} />
+						<Route path="/summary" element={<SummaryPage />} />
+					</Routes>
+				</Layout>
+			</BrowserRouter>
 		</div>
 	);
 }
---
+++ src/Components/Layout/Layout.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from "react";
+import NavBar from "../NavBar/NavBar";
+// import Footer from './Footer';
+
+interface LayoutProps {
+	children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+	return (
+		<div className="layout">
+			<NavBar />
+
+			<main className="main-content">{children}</main>
+			{/* <Footer /> */}
+		</div>
+	);
+};
+
+export default Layout;
