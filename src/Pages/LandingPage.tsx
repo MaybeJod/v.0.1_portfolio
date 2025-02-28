792ce4ac6c99ab99c574d33181da31eb11bf21b6
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-02-28 16:14:43 +0100

    feat: added about me

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,13 +1,14 @@
 import "./LandingPage.css";
-
 import Hero from "./LandingPageComponents/Hero";
 import Projects from "./LandingPageComponents/Projects";
+import About from "./LandingPageComponents/About";
 
 export default function LandingPage() {
 	return (
 		<>
 			<Hero />
 			<Projects />
+			<About />
 		</>
 	);
 }
---
+++ src/Pages/LandingPageComponents/About.tsx
@@ -0,0 +1,42 @@
+import tempJod from "../../Icons/temp_jod.jpg";
+
+export default function About() {
+	return (
+		<section id="about">
+			<div className="page-container">
+				<h2 className="about-heading">
+					Designer,
+					<br />
+					Developer,
+					<br />
+					and
+					<span className="demure"> Definitely wingin it</span> /
+				</h2>
+				<div className="about-card">
+					<div className="about-text">
+						<p>
+							Hi! I'm Jod — a Designer, a Developer, and a professional
+							certified expert at Winging It. I combine clean design, solid
+							code, and a sprinkle of chaos to bring ideas to life.
+						</p>
+						<p>
+							Every great project starts with a solid idea, a bit of Googling,
+							and just enough overconfidence to keep things interesting. Let's
+							build something amazing together!
+						</p>
+						<p>
+							When I'm not immersed in development and design, you can find me
+							playing badminton, eating banger food or tending to my cherished
+							houseplants
+						</p>
+					</div>
+					<img
+						src={tempJod}
+						alt="profile illustration of jod"
+						className="about-img"
+					/>
+				</div>
+			</div>
+		</section>
+	);
+}
