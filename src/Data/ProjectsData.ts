b850d5886ac1a6f70f94ec96ef27ce7bcba730c9
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-03-13 09:45:55 +0100

    fix: projects can use jps

--- src/Data/ProjectsData.ts
+++ src/Data/ProjectsData.ts
@@ -1,3 +1,5 @@
+import jod from "../Icons/temp_jod.jpg";
+
 interface Project {
 	title: string;
 	tag: string[];
@@ -20,7 +22,7 @@ export const ProjectsData: Project[] = [
 	project(
 		"V.0 Portfolio",
 		["Web Development", "Web Design"],
-		"https://images.unsplash.com/photo-1730121108301-65fe6cefb48d?q=80&w=3263&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		jod,
 		"https://github.com/MaybeJod/v.0_portfolio",
 		"lorem10"
 	),
