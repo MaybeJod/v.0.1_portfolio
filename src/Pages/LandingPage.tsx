706b2948a5ca8ba213d2e6d6833efada9b1e1589
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-03-19 13:11:57 +0100

    fix: remove testimonial section

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -2,7 +2,7 @@ import "./LandingPage.css";
 import Hero from "./LandingPageComponents/Hero";
 import Projects from "./LandingPageComponents/Projects";
 import About from "./LandingPageComponents/About";
-import Testimonial from "./LandingPageComponents/Testimonial";
+// import Testimonial from "./LandingPageComponents/Testimonial";
 import Contact from "./LandingPageComponents/Contact";
 import Footer from "./LandingPageComponents/Footer";
 import Header from "./LandingPageComponents/Header";
@@ -14,7 +14,7 @@ export default function LandingPage() {
 			<Hero />
 			<Projects />
 			<About />
-			<Testimonial />
+			{/* <Testimonial /> */}
 			<Contact />
 			<Footer />
 		</>
