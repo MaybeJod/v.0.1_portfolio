da55699c0e4bc87b9c8b2551395d9cab5d9766c4
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-03-21 14:27:57 +0100

    fix: change base to '/'

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/v.0.1_portfolio",
+	base: "/",
+	server: {
+		host: true,
+	},
 });
