31ff93316972c94fd65f1fb2750ec602e6f3fd2a
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-03-21 14:38:55 +0100

    fix: added back '/'

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/v.0.1_portfolio",
+	base: "/",
 	server: {
 		host: true,
 	},
